2b07c5e8c54d3fd027289720bc373ccba4e36cdc
unknown <deadmarvald@gmail.com>
2022-04-11 22:54:21 +0300

    Лабораторна робота №3.2(А) ПКР

---
+++ UT3.2A/UT3.2A.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR3.2A/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UT32A
+{
+	TEST_CLASS(UT32A)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			int N = 3;
+			Student* s = new Student[N];
+
+			s[0].inf = 5;
+			s[1].inf = 5;
+			s[2].inf = 5;
+			s[0].kurs = 1;
+			s[1].kurs = 1;
+			s[2].kurs = 1;
+			s[0].secondname = "1";
+			s[0].secondname = "1";
+			s[0].secondname = "1";
+
+			t = BinSearch(s, N, 5, 1, "1");
+
+			Assert::AreEqual(t, -1.);
+		}
+	};
+}
